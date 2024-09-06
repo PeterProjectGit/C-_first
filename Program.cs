cba11231dd8f6f531598050af6951e1fc948b485
Peti <petya.antal@gmail.com>
2024-09-06 09:13:01 +0200

    alma,

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace C__first;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Az első argumentum: "+ args[0]);
+        
+        Console.Write("1. sor;");
+        System.Console.WriteLine("2.sor");
+    }
+}
